e6078023e0585abbb9674b3c4cdd7a6a1754fed7
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2024-10-11 15:25:50 +0200

    Injected mockOut and in

--- src/test/java/io/gocklkatz/cameltesting/CamelTestingTest.java
+++ src/test/java/io/gocklkatz/cameltesting/CamelTestingTest.java
@@ -1,5 +1,8 @@
 package io.gocklkatz.cameltesting;
 
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit5.CamelTestSupport;
@@ -8,6 +11,12 @@ import org.junit.jupiter.api.Test;
 
 class CamelTestingTest extends CamelTestSupport {
 
+    @EndpointInject("mock:out")
+    private MockEndpoint mockOut;
+
+    @Produce("direct:in")
+    private ProducerTemplate in;
+
     @Override
     protected RouteBuilder createRouteBuilder() throws Exception {
         return new SimpleTransformRouteBuilder();
@@ -15,10 +24,12 @@ class CamelTestingTest extends CamelTestSupport {
 
     @Test
     public void testPayloadIsTransformed() throws InterruptedException {
-        MockEndpoint mockOut = getMockEndpoint("mock:out");
+        //MockEndpoint mockOut = getMockEndpoint("mock:out");
         mockOut.setExpectedMessageCount(1);
         mockOut.message(0).body().isEqualTo("Modified: Cheese");
-        template.sendBody("direct:in", "Cheese");
+        //template.sendBody("direct:in", "Cheese");
+        in.sendBody("Cheese");
         mockOut.assertIsSatisfied();
+        //MockEndpoint.assertIsSatisfied(context);
     }
 }
