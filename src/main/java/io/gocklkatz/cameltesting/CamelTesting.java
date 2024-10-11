b6dc09a77e56b4907b4d4415582db7fe48a2bc6a
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2024-10-11 14:25:42 +0200

    Initial setup

---
+++ src/main/java/io/gocklkatz/cameltesting/CamelTesting.java
@@ -0,0 +1,5 @@
+package io.gocklkatz.cameltesting;
+
+public class CamelTesting {
+
+}
---
+++ src/test/java/io/gocklkatz/cameltesting/CamelTestingTest.java
@@ -0,0 +1,7 @@
+package io.gocklkatz.cameltesting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CamelTestingTest {
+
+}
